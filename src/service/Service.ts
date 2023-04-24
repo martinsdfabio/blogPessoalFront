8cb2adfdd4f2e36f392ceb9191a5f9025aacafd1
Fabio Martins <generation@mail.com>
2023-04-24 19:34:02 -0300

    Postagem

--- src/service/Service.ts
+++ src/service/Service.ts
@@ -1,20 +1,40 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://blogpessoal-6qar.onrender.com'
-})
+  baseURL: 'https://blogdothiagofaccipieri.onrender.com/',
+});
 
-export const login = async(url: any, dados: any, setDados: any) => {
-  const resposta = await api.post(url,dados)
-  setDados(resposta.data.token)
-}
+export const login = async (url: any, dados: any, setDados: any) => {
+  const resposta = await api.post(url, dados);
+  setDados(resposta.data.token);
+};
 
-export const cadastrarUsuario = async(url: any, dados: any, setDados: any) => {
-  const resposta = await api.post(url,dados)
-  setDados(resposta.data)
+export const cadastrarUsuario = async (url: any, dados: any, setDados: any) => {
+  const resposta = await api.post(url, dados);
+  setDados(resposta.data);
+};
+
+// a tipagem do headers seria um Object
+export const getAll = async (url: any, setDados: any, headers: any) => {
+  const resposta = await api.get(url, headers);
+  setDados(resposta.data);
+};
+
+export const getId = async (url: any, setDados: any, headers: any) => {
+  const resposta = await api.get(url, headers);
+  setDados(resposta.data);
+};
+
+export const post = async (url: any, dados: any, setDados: any, headers: any) => {
+  const resposta = await api.post(url, dados, headers);
+  setDados(resposta.data);
+};
+
+export const put = async (url: any, dados: any, setDados: any, headers: any) => {
+  const resposta = await api.put(url, dados, headers);
+  setDados(resposta.data);
+};
+
+export const deleteId = async(url: any, headers: any) => {
+  await api.delete(url, headers)
 }
-// a tipagem do headers seria Object
-export const getAll = async(url: any, setDados: any, headers: any) => {
-const resposta = await api.get(url, headers)
-setDados(resposta)
-} 
